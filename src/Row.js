b297cba9230af1c526ac757fcc329d01d39530dd
waleedhassan081 <waleed@almoosa.tech>
2020-10-11 09:54:59 +0400

    netflix clone is ready

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
-
+    const [trailerUrl, setTrailerUrl] = useState("");
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
@@ -12,7 +14,25 @@ function Row({ title, fetchUrl }) {
         }
         fetchData();
     }, [fetchUrl]);
-
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.name || "")
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get("v"));
+                })
+                .catch((error) => console.log(error));
+        }
+    };
     return (
         <div className="row">
             <h2>{title}</h2>
@@ -27,10 +47,12 @@ function Row({ title, fetchUrl }) {
                             key={movie.id}
                             src={base_url + "" + movie.poster_path}
                             alt={movie.original_name}
+                            onClick={() => handleClick(movie)}
                         />
                     );
                 })}
             </div>
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
         </div>
     );
 }
